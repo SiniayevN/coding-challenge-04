ff9d422b5b88fd72971b6235386b842f9b43caba
Nikita Siniayev <ns21@usf.edu>
2025-09-18 09:51:56 -0400

    array created

---
+++ cc_4.js
@@ -0,0 +1,9 @@
+let products = [
+    { name: "Laptop", category: "Electronics", price: 850, inventory: 30 },
+    { name: "Potato", category: "Groceries", price: 600, inventory: 50 },
+    { name: "Hoodie", category: "Apparel", price: 60, inventory: 100 },
+    { name: "Vacuum", category: "Household", price: 200, inventory: 20 },
+    { name: "ChapStick", category: "Default", price: 5, inventory: 200 },
+]
+
+//Step 2 done
