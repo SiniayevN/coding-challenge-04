fa0ad69840bef9fcfae78fa04e1f2530840d011e
Nikita Siniayev <ns21@usf.edu>
2025-09-18 10:00:31 -0400

    updated and step 3

--- cc_4.js
+++ cc_4.js
@@ -1,9 +1,30 @@
 let products = [
     { name: "Laptop", category: "Electronics", price: 850, inventory: 30 },
-    { name: "Potato", category: "Groceries", price: 600, inventory: 50 },
     { name: "Hoodie", category: "Apparel", price: 60, inventory: 100 },
+    { name: "Potato", category: "Groceries", price: 600, inventory: 50 },
     { name: "Vacuum", category: "Household", price: 200, inventory: 20 },
     { name: "ChapStick", category: "Default", price: 5, inventory: 200 },
 ]
-
 //Step 2 done
+
+for (let product of products) {
+    let discount = 0;
+    switch (product.category) {
+        case "Electronics":
+            discount = .2;
+            break;
+
+        case "Apparel":
+            discount = .15;
+            break;
+
+            case "Household":
+                case "Groceries":
+                    discount = .1;
+                    break;
+                    
+        default:
+            discount = 0;
+    }
+}
+
